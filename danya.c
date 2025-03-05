f7aaf36180b05e2313eb3c7e2eee83e216cb4806
Leon-4242 <leon.zazovskii@math.msu.ru>
2025-03-05 18:16:51 +0300

    upd

--- danya.c
+++ danya.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 
 int cmp(const void*, const void*);
+void swap(int*, int*);
+void swapArray(int (*)[7], int (*)[7]);
 
 int main(void) {
 	FILE *in, *out;
-	int max7[7], curr7[7], storage[14];
-	int sumMax, sum, curr, i, j, k, flag, f, counter;
+	int max1[7], max2[7], max3[7], curr7[7];
+	int sumMax1, sumMax2, sumMax3, sum, curr, i, j, k, flag, num;
 	
-	k = 0; f = flag = 1; counter = 0;
+	num = 1; k = 0; flag = 1;
 	
 	in = fopen("data.dat", "r");
 	if (in == NULL) {
@@ -22,8 +24,8 @@ int main(void) {
 		if (k == 0) {
 			curr7[0] = curr;
 			sum = curr;
-			max7[0] = curr;
-			sumMax = curr;
+			max1[0] = curr;
+			sumMax1 = curr;
 			k++;
 		} else if (k < 7) {
 			flag = 1;
@@ -31,8 +33,8 @@ int main(void) {
 				if (curr == curr7[0]) {
 					curr7[0] = curr;
 					sum = curr;
-					max7[0] = curr;
-					sumMax = curr;
+					max1[0] = curr;
+					sumMax1 = curr;
 					k = 1;
 					flag = 0;
 					break;
@@ -41,16 +43,11 @@ int main(void) {
 			if (flag) {
 				curr7[k] = curr;
 				sum += curr;
-				max7[k] = curr;
-				sumMax += curr;
+				max1[k] = curr;
+				sumMax1 += curr;
 				k++;
 			}
 		} else {
-            if (f) {
-                counter = 1;
-                for (i = 0; i < 7; ++i) storage[7*(counter-1) + i] = curr7[i];
-                f = 0;
-            }
 			flag = 1;
 			sum = 0;
 			for (i = 0; i < 6; ++i) {
@@ -69,14 +66,49 @@ int main(void) {
 			}
 			
 			if (flag) {
-                if (sum > sumMax) {
-                    for (i = 0 ; i < 7 ; ++i) max7[i] = curr7[i];
-                    sumMax = sum;
+                if (num == 1) {
+                    for (i = 0; i < 7; ++i) max2[i] = curr7[i];
+                    sumMax2 = sum;
+                    if (sumMax2 > sumMax1) {
+                        swapArray(&max1, &max2);
+                        swap(&sumMax1, &sumMax2);
+                    }
+                } else if (num == 2) {
+
+                    for (i = 0; i < 7; ++i) max3[i] = curr7[i];
+                    sumMax3 = sum;
+
+                    if (sumMax3 > sumMax2) {
+                        swapArray(&max2, &max3);
+                        swap(&sumMax2, &sumMax3);
+
+                        if (sumMax2 > sumMax1) {
+                            swapArray(&max1, &max2);
+                            swap(&sumMax1, &sumMax2);
+                        }
+
+                    }
+                } else {
+                    if (sum > sumMax3 && sum < sumMax2) {
+                        for (i = 0; i < 7; ++i) max3[i] = curr7[i];
+                        sumMax3 = sum;
+                    } else if (sum > sumMax2 && sum < sumMax1) {
+                        swapArray(&max2, &max3);
+                        swap(&sumMax2, &sumMax3);
+                        for (i = 0; i < 9; ++i) max2[i] = curr7[i];
+                        sumMax2 = sum;
+                    } else if (sum > sumMax1) {
+                        swapArray(&max2, &max3);
+                        swap(&sumMax2, &sumMax3);
+
+                        swapArray(&max1, &max2);
+                        swap(&sumMax1, &sumMax2);
+
+                        for(i = 0; i < 7; ++i) max1[i] = curr7[i];
+                        sumMax1 = sum;
+                    }
                 }
-                if (counter < 2) {
-                    counter++;
-                    for (i = 0; i < 7; ++i) storage[7*(counter-1) + i] = curr7[i];
-                } else if (counter == 2) counter = 3;
+                num++;
 			}
 		}
 	}
@@ -87,15 +119,24 @@ int main(void) {
 	
 	if (k != 7) {
 		fprintf(out, "NO");
-	} else if (counter < 3) {
-        qsort(storage, 7, sizeof(int), cmp);
-        qsort(storage + 7, 7, sizeof(int), cmp);
-        for (i = 0; i < 7; ++i) fprintf(out, "%d ", storage[i]);
+	} else if (num == 1) {
+        qsort(max1, 7, sizeof(int), cmp);
+        for (i = 0; i < 7; ++i) fprintf(out, "%d ", max1[i]);
+    } else if (num == 2){
+        qsort(max1, 7, sizeof(int), cmp);
+        qsort(max2, 7, sizeof(int), cmp);
+        for (i = 0; i < 7; ++i) fprintf(out, "%d ", max1[i]);
         fprintf(out, "\n");
-        for (i = 0; i < 7; ++i) fprintf(out, "%d ", storage[i+7]);
+        for (i = 0; i < 7; ++i) fprintf(out, "%d ", max2[i]);
     } else {
-        qsort(max7, 7, sizeof(int), cmp);
-        for (i = 0; i < 7; ++i) fprintf(out, "%d ", max7[i]);
+        qsort(max1, 7, sizeof(int), cmp);
+        qsort(max2, 7, sizeof(int), cmp);
+        qsort(max3, 7, sizeof(int), cmp);
+        for (i = 0; i < 7; ++i) fprintf(out, "%d ", max1[i]);
+        fprintf(out, "\n");
+        for (i = 0; i < 7; ++i) fprintf(out, "%d ", max2[i]);
+        fprintf(out, "\n");
+        for (i = 0; i < 7; ++i) fprintf(out, "%d ", max3[i]);
     }
 		
 	fclose(out);
@@ -109,4 +150,15 @@ int cmp (const void* aa, const void* bb) {
 	if (a > b) return 1;
 	if (a < b) return -1;
 	return 0;
+}
+
+void swap(int* a, int* b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+void swapArray(int (*A)[7], int (*B)[7]) {
+    int i;
+    for (i = 0; i < 7; ++i) swap((*A)+i, (*B)+i);
 }
